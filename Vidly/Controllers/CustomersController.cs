b4c3a05c0372ad6a5dc4ab08399a09876870943c
Joseph Hiwatig <jhiwatig.dev@gmail.com>
2021-01-23 18:54:30 -0500

    Replaced Hard Coded Customers with _context calls

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -9,27 +9,43 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private VidlyContext _context;
+
+        public CustomersController()
+        {
+            _context = new VidlyContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith"},
-                new Customer { Id = 2, Name =  "Mary Williams" }
-            };
+            //var customers = new List<Customer>
+            //{
+            //    new Customer { Id = 1, Name = "John Smith"},
+            //    new Customer { Id = 2, Name =  "Mary Williams" }
+            //};
+
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customers = new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith"},
-                new Customer { Id = 2, Name =  "Mary Williams" }
-            };
+            //var customers = new List<Customer>
+            //{
+            //    new Customer { Id = 1, Name = "John Smith"},
+            //    new Customer { Id = 2, Name =  "Mary Williams" }
+            //};
 
-            var selectedCustomer = customers.Where(customer => customer.Id == id).FirstOrDefault();
+            //var selectedCustomer = customers.Where(customer => customer.Id == id).FirstOrDefault();
+
+            var selectedCustomer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (selectedCustomer == null)
                 return HttpNotFound();
